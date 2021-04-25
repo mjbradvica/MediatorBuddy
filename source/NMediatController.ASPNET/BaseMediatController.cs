28689f1ded6cd23c92138add1aeb5d03d144bcca
Michael Bradvica <mjbradvica@protonmail.com>
2021-04-25 14:02:47 -0700

    base projects

---
+++ source/NMediatController.ASPNET/BaseMediatController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMediatController.ASPNET
+{
+    public abstract class BaseMediatController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        protected BaseMediatController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}

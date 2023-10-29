c7f9c7b0d736824c041f7ed43a745076f06c4f77
Michael B <mjbradvica@gmail.com>
2023-10-29 10:45:48 -0400

    sample update

---
+++ samples/MediatorBuddy.Samples.Api/Exceptions/GlobalExceptionOccurredHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace MediatorBuddy.Samples.Api.Exceptions
+{
+    public class GlobalExceptionOccurredHandler : INotificationHandler<GlobalExceptionOccurred>
+    {
+        private readonly ILogger _logger;
+
+        public GlobalExceptionOccurredHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(GlobalExceptionOccurred notification, CancellationToken cancellationToken)
+        {
+            _logger.LogError(notification.Exception, message: "Global exception at {dateTime}", notification.DateTime);
+
+            return Task.CompletedTask;
+        }
+    }
+}
